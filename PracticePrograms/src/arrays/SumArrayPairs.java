e582277dbad98afe62b7623310e5865f35b699f9
Utkarsh Srivastava <87950519+Utkarsh630@users.noreply.github.com>
2024-10-19 01:07:01 -0700

    Update SumArrayPairs.java

--- PracticePrograms/src/arrays/SumArrayPairs.java
+++ PracticePrograms/src/arrays/SumArrayPairs.java
@@ -8,7 +8,7 @@ public class SumArrayPairs {
 
     public static void sumOfPairs(int [] arr, int target){
         for(int i=0;i<arr.length-1;i++){
-             for(int j=1;j<arr.length;j++){
+             for(int j=i+1;j<arr.length;j++){
                  if(arr[i]+arr[j]==target){
                      System.out.println("{" + arr[i] + "," + arr[j]+"}");
                  }
